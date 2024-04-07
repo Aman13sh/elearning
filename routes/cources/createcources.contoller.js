4c452522c182e1f99d6485e7836bed43064e79c8
Aman14sh <amasharma862@gmail.com>
2024-04-07 23:11:52 +0530

    delete routes

--- routes/cources/createcources.contoller.js
+++ routes/cources/createcources.contoller.js
@@ -5,7 +5,7 @@ async function createcources(req,res){
     const {courseId,c_name,price,creator,discription,email} = req.body;
 
     
-    if(!courseId || !c_name || !price || creator || discription || email){
+    if(!courseId || !c_name || !price || !creator || !discription || !email){
       return res.status(500).json({sucess:false,message:"All details not filled"});
     }
     const client = await pool.connect();
@@ -66,6 +66,60 @@ async function createcources(req,res){
     }
 
 }
+async function  deletecources(req,res){
+  const {courseId,email}=req.body;
+  if(!courseId ){
+    return res.status(500).json({sucess:false,message:"All details not filled"});
+  }
+  const client = await pool.connect();
+  var isAdmin;
+  try{
+      //checking user is Admin or not
+      const query = {
+        name: 'fetch-user',
+        text: 'SELECT isAdmin FROM userDetails WHERE email = $1',
+        values: [email],
+      }
+      isAdmin = await client.query(query);
+    }
+    catch(error){
+      console.log(error);
+      return res.status(500).json({sucess:false,message:"error in query fetching"});
+    }
+    var courseIDPresence;
+    try{
+      //checking course is already present or not
+      const query = {
+        name: 'fetch-cources',
+        text: 'SELECT * FROM coursedetails WHERE id_s = $1',
+        values: [courseId],
+      }
+      courseIDPresence= await client.query(query);
+    }
+    catch(error){
+      console.log(error);
+      return res.status(500).json({sucess:false,message:"error in query fetching"});
+    }
+
+    if(courseIDPresence.rows.length == 0){
+      return res.status(200).json({sucess:false,message:"course not present"});
+   } 
+   const text = 'DELETE FROM coursedetails where id_s=$1';
+   const values = [courseId];
+   try{
+    await client.query(text, values)
+    return res.status(200).json({sucess:true,message:"User is created"});
+    }
+    catch(error){
+      console.log(error);
+        return res.status(400).json({sucess:false,message:"Course is deleted"});
+    }
+  finally {
+    client.release();
+  }
+
+}
 module.exports={
     createcources,
+    deletecources,
 }
--- routes/cources/createcources.router.js
+++ routes/cources/createcources.router.js
@@ -3,6 +3,7 @@ const express = require('express');
 const createcourcesRouter=express.Router();
 
 const {createcources}=require('./createcources.contoller');
+const {deletecources}=require('./createcources.contoller');
 createcourcesRouter.post('/createcources',createcources);
-
+createcourcesRouter.put('/deletecreatecources',deletecources);
 module.exports=createcourcesRouter;
